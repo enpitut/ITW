4aff0ca374736e8ccc739438abae8776b8ee0c59
FLT <ft4613st@gmail.com>
2015-08-21 23:27:30 +0900

    bootstrap/サイドメニューのスニペットです。参考までに ただ、サイドメニューに関してはsldrを使ったほうが格好良くなると思います

---
+++ menu.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>サイドメニューテスト</title>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+<body>
+<div class="col-lg-2 col-md-2 col-sm-2">
+	<ul class="nav nav-pills nav-stacked navbar-fixed">
+		<li role="presentation"><a href="#">地図</a></li>
+		<li role="presentation"><a href="#">オプション</a></li>
+		<li role="presentation"><a href="#">ログアウト</a></li>
+	</ul>
+</div>
+</body>
+</HTML>
