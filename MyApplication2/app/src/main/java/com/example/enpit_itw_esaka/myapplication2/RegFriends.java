fe83d6f81a94a47599d440b45117a42a66d96c8f
walk8243 <walk8243@gmail.com>
2015-11-06 09:59:26 +0900

    QRコードをJPEG方式で保存出来るようになりました。

--- MyApplication2/app/src/main/java/com/example/enpit_itw_esaka/myapplication2/RegFriends.java
+++ MyApplication2/app/src/main/java/com/example/enpit_itw_esaka/myapplication2/RegFriends.java
@@ -1,20 +1,38 @@
 package com.example.enpit_itw_esaka.myapplication2;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class RegFriends extends Activity {
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Hashtable;
+
+public class RegFriends extends Activity implements OnClickListener {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.regfriends);
-        /*
-        //受け取ったインテントを取得
-        Intent intent = getIntent();
-        showIntentData(intent);
-        */
+        createQRcode();
     }
 
     //バーにボタンを追加する
@@ -26,7 +44,7 @@ public class RegFriends extends Activity {
         return true;
     }
 
-    //バーのボタンを押したとき
+    //バーのボタンを押されたとき
     @Override
     public boolean onMenuItemSelected(int featureId, MenuItem item){
         switch(item.getItemId()) {
@@ -43,53 +61,75 @@ public class RegFriends extends Activity {
     //ボタンを押したとき
     @Override
     public void onClick(View v){
-        switch(v.getId()){
+        switch (v.getId()){
             case R.id.button2:
                 //処理を書く
-                Log.v("event","push button!");
-                Toast.makeText(this, "ボタンがクリックされました", Toast.LENGTH_LONG).show();
+                Log.v("event", "push button!");
+                Toast.makeText(this, "ボタンがクリックされました", Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
         }
     }
 
-    /*
-    private void showIntentData(Intent intent){
-        //QRコードの作成
-        try {
-            //インテントからBundleを取得
-            Bundle bundle = intent.getExtras();
+    private void createQRcode() {
+        Bitmap qr = null;
+        try{
+            qr = createQRCodeByZxing("http://google.co.jp", 400);
+        }catch (WriterException e){
+            Log.d("createQRCode", "error:", e);
+        }
 
-            //ユーザIDオブジェクトを取得
-            String number = (String) bundle.get("userID");
+        try{
+            File root = Environment.getExternalStorageDirectory();
 
-            QRCodeWriter writer = new QRCodeWriter();
-            //エンコード
-            BitMatrix bm = null;
-            bm = writer.encode(number, BarcodeFormat.QR_CODE, 100, 100);
-            //ピクセルを作る
-            int width = bm.getWidth();
-            int height = bm.getHeight();
-            int[] pixels = new int[width * height];
-            //データがあるところだけ黒にする
-            for(int y=0; y<height; y++){
-                int offset = y*width;
-                for(int x=0;x<width; x++){
-                    pixels[offset+x] = bm.get(x, y) ? Color.BLACK : Color.WHITE;
-                }
-            }
-            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-            bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+            //日付でファイル名を作成
+            Date mDate = new Date();
+            SimpleDateFormat fileName = new SimpleDateFormat("yyyyMMdd_HHmmSS");
 
-            //QRコードを表示する
-            ImageView image = new ImageView(this);
-            image.setImageBitmap(bitmap);
-            setContentView(image);
+            //保存処理開始
+            FileOutputStream fos = null;
+            fos = new FileOutputStream(new File(root, fileName.format(mDate)+".jpg"));
 
-        } catch (WriterException e) {
-            e.printStackTrace();
+            //jpegで保存
+            qr.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+
+            //保存処理終了
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.e("Error", ""+e.toString());
+        } catch (IOException e) {
+            Log.e("Error", "" + e.toString());
         }
     }
-    */
+
+    //contentsは変換する文字列、sizeは出力するQRコードの大きさ
+    public Bitmap createQRCodeByZxing(String contents, int size) throws WriterException{
+        //QRコードをエンコードするクラス
+        QRCodeWriter writer = new QRCodeWriter();
+
+        //異なる型のあたいを入れるためgenericは使えない
+        Hashtable encodeHint = new Hashtable();
+
+        //日本語を扱うためにシフトJISを指定
+        encodeHint.put(EncodeHintType.CHARACTER_SET, "shiftjis");
+        //エラー修復レベルを指定
+        encodeHint.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+        BitMatrix qrCodeData = writer.encode(contents, BarcodeFormat.QR_CODE, size, size, encodeHint);
+
+        //QRコードのbitmap画像を作成
+        Bitmap bitmap = Bitmap.createBitmap(size, size, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.argb(255, 255, 255, 255));
+        for(int x=0; x<qrCodeData.getWidth(); x++) {
+            for(int y=0; y<qrCodeData.getHeight(); y++){
+                if(qrCodeData.get(x, y) == true){
+                    bitmap.setPixel(x, y, Color.argb(255, 0, 0, 0));//trueはBlack
+                }else{
+                    bitmap.setPixel(x, y, Color.argb(255, 255, 255, 255));//falseはWhite
+                }
+            }
+        }
+        return bitmap;
+    }
 }
