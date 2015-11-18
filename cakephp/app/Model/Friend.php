142b3d771871416f6174bc52939bfe2c5ab8c85a
123456789aaaaa <s1520635@u.tsukuba.ac.jp>
2015-11-18 17:44:29 +0900

    友達登録のエラー修正．今後は登録された友達を表示するシステムを実装

--- cakephp/app/Model/Friend.php
+++ cakephp/app/Model/Friend.php
@@ -7,17 +7,14 @@
  */
 
 class Friend extends AppModel {
-    public $name1 = 'id';
-
-    public $name2 = 'friendsid';
-	public $n ='id';
 
 
-    public function beforeSave($options = array())
-    {
-        $this->data['id']['friendsid'] = AuthComponent::password($this->data['id']['friendsid'] );
-        return true;
-    }
+
+   // public function beforeSave($options = array())
+ //   {
+      //  $this->data[$user['id']]['friendsid'] = AuthComponent::password($this->data[$user['id']]['friendsid'] );
+     //   return true;
+ //   }
 
 }
 
