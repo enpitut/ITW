e202ce7cc55709dbbf57e9a28321c9366ee01fb0
walk8243 <walk8243@gmail.com>
2015-11-23 21:45:28 +0900

    Update MyAsyncTask.java

--- MyApplication2/app/src/main/java/com/example/enpit_itw_esaka/myapplication2/MyAsyncTask.java
+++ MyApplication2/app/src/main/java/com/example/enpit_itw_esaka/myapplication2/MyAsyncTask.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
@@ -15,21 +16,20 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.awt.font.TextAttribute;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by yoshikawa on 2015/11/01.
- */
-public class MyAsyncTask extends AsyncTask<String, Integer, Integer> implements DialogInterface.OnCancelListener {
+public class MyAsyncTask extends AsyncTask<String, Integer, Integer> implements DialogInterface.OnCancelListener{
     final String TAG = "MyAsyncTask";
 
     private Activity m_Activity;
 
     private static final String url = "http://192.168.1.52/position_registor.php";
+    private static final String urlF = "http://192.168.1.52/friend_registor.php";
 
     //クライアント設定
     HttpClient httpclient = new DefaultHttpClient();
@@ -40,97 +40,111 @@ public class MyAsyncTask extends AsyncTask<String, Integer, Integer> implements 
     ByteArrayOutputStream byteArrayOutputStream;
     HttpResponse res = null;
 
-    public MyAsyncTask(Activity activity) {
+    public MyAsyncTask(Activity activity){
         this.m_Activity = activity;
     }
 
-
     @Override
-    protected void onPreExecute() {
+    protected void onPreExecute(){
         Log.d(TAG, "onPreExecute");
     }
 
     @Override
-    protected Integer doInBackground(String... contents) {
-        Log.d(TAG, "doInBackground - " + contents[0] + contents[1]);
-        HttpClient httpClient = new DefaultHttpClient();
-        HttpPost post = new HttpPost(url);
+    protected Integer doInBackground(String... contents){
+        if(contents.length == 2) {
+            Log.d(TAG, "doInBackground - " + contents[0] + contents[1]);
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urlF);
 
-        ArrayList <NameValuePair> params = new ArrayList<NameValuePair>();
-        params.add( new BasicNameValuePair("lat", contents[0]));
-        params.add(new BasicNameValuePair("lon", contents[1]));
-        params.add(new BasicNameValuePair("date", contents[2]));
-        params.add(new BasicNameValuePair("user_id", contents[3]));
+            ArrayList <NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("friend_id", contents[0]));
+            params.add(new BasicNameValuePair("user_id", contents[1]));
 
-        try {
-            post.setEntity(new UrlEncodedFormEntity(params, "utf-8"));
-            res = httpClient.execute(post);
+            try{
+                post.setEntity(new UrlEncodedFormEntity(params, "utf-8"));
+                res = httpClient.execute(post);
 
-            byteArrayOutputStream = new ByteArrayOutputStream();
-            res.getEntity().writeTo(byteArrayOutputStream);
+                byteArrayOutputStream = new ByteArrayOutputStream();
+                res.getEntity().writeTo(byteArrayOutputStream);
+            }catch (Exception e){
+                Log.v("MyAsyncTask", e.toString());
+                return -1;
+            }
+        }else if(contents.length == 4){
+            Log.d(TAG, "doInBackground - " + contents[0] + contents[1]);
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost post = new HttpPost(url);
 
-        }
-        catch(Exception e){
-            Log.v("MyAsyncTask", e.toString());
+            ArrayList <NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("lat", contents[0]));
+            params.add(new BasicNameValuePair("lon", contents[1]));
+            params.add(new BasicNameValuePair("date", contents[2]));
+            params.add(new BasicNameValuePair("user_id", contents[3]));
+
+            try{
+                post.setEntity(new UrlEncodedFormEntity(params, "utf-8"));
+                res = httpClient.execute(post);
+
+                byteArrayOutputStream = new ByteArrayOutputStream();
+                res.getEntity().writeTo(byteArrayOutputStream);
+            }catch (Exception e){
+                Log.v("MyAsyncTask", e.toString());
+                return -1;
+            }
+        }else{
+            Log.e("Error", "情報数エラーです。");
             return -1;
         }
 
         return Integer.valueOf(res.getStatusLine().getStatusCode());
-
     }
 
     @Override
-    protected void onProgressUpdate(Integer... values) {
+    protected void onProgressUpdate(Integer... values){
         Log.d(TAG, "onProgressUpdate - " + values[0]);
-
     }
 
     @Override
-    protected void onCancelled() {
+    protected void onCancelled(){
         Log.d(TAG, "onCancelled");
-
     }
 
     @Override
-    protected void onPostExecute(Integer result) {
+    protected void onPostExecute(Integer result){
         Log.d(TAG, "onPostExecute - " + result);
 
-        if(result == -1) {//通信に失敗
+        if(result == -1){
+            //通信に失敗
             Log.d(TAG, "communication error");
             return;
-        }
-        else{
-            Log.d(TAG,"Status : " + res.getStatusLine().getStatusCode());
+        }else{
+            Log.d(TAG, "Status : " + res.getStatusLine().getStatusCode());
         }
         //サーバーからの応答を取得
-        if(res.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
-        {
-//デバッグ用にリザルトコードをTextViewに表示させておく
+        if(res.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            //デバッグ用にリザルトコードをTextViewに表示させておく
             /*
             TextView tv = (TextView) this.m_Activity.findViewById(R.id.textView1);
-            tv.setText(""+result);
-
-            tv.setText(result+"\n"+byteArrayOutputStream.toString());
-*/
-//サーバーから受けとった文字列の処理
-            //Toast.makeText(this.m_Activity, "結果 : " + byteArrayOutputStream.toString(),Toast.LENGTH_LONG).show();
-            Log.d(TAG, "サーバからの応答 : " + byteArrayOutputStream.toString());
+            tv.setText("" + result);
+            tv.setText(result + "\n" + byteArrayOutputStream.toString());
+            */
+            //サーバーから受け取った文字列の処理
+            //Toast.makeText(this.m_Activity, "結果 : " + byteArrayOutputStream.toString(), Toast.LENGTH_LONG).show();
+            Log.d("TAG", "サーバからの応答 : " + byteArrayOutputStream.toString());
             /*
             if(byteArrayOutputStream.toString().equals("1")){
-                Toast.makeText(this.m_Activity, "[ここには処理１] ", Toast.LENGTH_LONG).show();
+                Toast.makeText(this.m_Activity, "[ここには処理1]", Toast.LENGTH_LONG).show();
             }else{
-                Toast.makeText(this.m_Activity, "[ここには処理２] ", Toast.LENGTH_LONG).show();
+                Toast.makeText(this.m_Activity, "[ここには処理2]", Toast.LENGTH_LONG).show();
             }
             */
-        }
-        else
-        {
-            Toast.makeText(this.m_Activity, "[error]: "+response.getStatusLine(), Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this.m_Activity, "[error]: " + response.getStatusLine(), Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
-    public void onCancel(DialogInterface dialog) {
+    public void onCancel(DialogInterface dialog){
         Log.d(TAG, "Dialog onCancel... calling cancel(true)");
         this.cancel(true);
     }
