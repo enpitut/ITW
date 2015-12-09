bbdd488db6f98e40a32a0523c0436e1caee6e73b
kein1592 <yskw1592@gmail.com>
2015-12-09 22:34:01 +0900

    親子登録も動作する形に修正

--- parent_registor.php
+++ parent_registor.php
@@ -15,22 +15,21 @@
       echo -1;
     }else{
       $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-      $check_sql = 'SELECT * FROM users WHERE user_id = :user_id';
+      $check_sql = 'SELECT * FROM users WHERE id = :user_id';
       $check_stmt = $dbh->prepare($check_sql);
       $check_stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
       $check_stmt->execute();
       $rc = $check_stmt->rowCount();
-      $fp = fopen("parentlog.txt", "a");
-      fwrite($fp, "rc = ".$rc."\r\n");
-      fclose($fp);
+
       if($rc == 0){
+      /*
         $insert_sql = 'INSERT INTO positions (id, parentid) values (:user_id, :parentid)';
         $stmt = $dbh->prepare($insert_sql);
         $stmt->bindValue(':parentid', $parentid, PDO::PARAM_INT);
         $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-        $stmt->execute();
+        $stmt->execute();*/
       }else{
-        $update_sql = 'UPDATE friends SET parentid = :parentid WHERE id=:user_id';
+        $update_sql = 'UPDATE users SET parentid = :parentid WHERE id=:user_id';
         $stmt = $dbh->prepare($update_sql);
         $stmt->bindValue(':parentid', $parentid, PDO::PARAM_INT);
         $stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
